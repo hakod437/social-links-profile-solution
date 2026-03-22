22774068f2cc2dc9cef2ca087bb34bda11398003
Halim <halim.sonou-agossou@epitech.eu>
2026-03-22 15:35:11 +0100

    refactor: restructure App component for improved layout and styling

--- social-links-profile-main/react-app/src/App.jsx
+++ social-links-profile-main/react-app/src/App.jsx
@@ -2,18 +2,22 @@ import './App.css'
 
 function App() {
   return (
-    <div className="parent-div">
-      <div className="child-div child-div--image">
-        <img className="profile-image" src="/assets/images/avatar-jessica.jpeg" alt="Jessica Randall" />
+    <div className="App">
+      <div className="parent-div">
+        <div className="child-div child-div--image">
+          <img className="profile-image" src="/assets/images/avatar-jessica.jpeg" alt="Jessica Randall" />
+        </div>
+        <div className="child-div name-location">
+          <span className="name">Jessica Randall</span>
+          <span className="location">London, United Kingdom</span>
+        </div>
+        <div className="child-div bio">"Front-end developer and avid reader."</div>
+        <div className="child-div link github">GitHub</div>
+        <div className="child-div link frontend-mentor">Frontend Mentor</div>
+        <div className="child-div link linkedin">LinkedIn</div>
+        <div className="child-div link twitter">Twitter</div>
+        <div className="child-div link instagram">Instagram</div>
       </div>
-      <div className="child-div name">Jessica Randall</div>
-      <div className="child-div location">London, United Kingdom</div>
-      <div className="child-div bio">Front-end developer and avid reader.</div>
-      <div className="child-div link github">GitHub</div>
-      <div className="child-div link frontend-mentor">Frontend Mentor</div>
-      <div className="child-div link linkedin">LinkedIn</div>
-      <div className="child-div link twitter">Twitter</div>
-      <div className="child-div link instagram">Instagram</div>
     </div>
   )
 }
